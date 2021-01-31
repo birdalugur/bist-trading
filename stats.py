bc12f29d4fd02c66bd2ae61d80666c6b73711f05
birdalugureren <birdalugureren@gmail.com>
2021-01-31 19:29:39 +0300

    update stats

--- stats.py
+++ stats.py
@@ -2,35 +2,42 @@ import pandas as pd
 
 data = pd.read_csv("bs_rol300_noint_5min_thr1_std.csv", parse_dates=['exit_time', 'entry_time'])
 
-long = (data['exit_price_1'] - data['entry_price_1']) * 100
-short = -(data['exit_price_2'] - data['entry_price_2']) * 100
-
-c_long = long.cumsum()
-c_short = short.cumsum()
-c_returns = c_long + c_short
-
-returns = long + short
-
-data['long'] = long
-data['short'] = short
-data['c_long'] = c_long
-data['c_short'] = c_short
-data['c_return'] = c_returns
-data['return'] = returns
-
-last_c_return = data.set_index(['entry_symbol_1', 'entry_symbol_2']).groupby(level=[0, 1])['c_return'].tail(1)
-number_of_trades = data.groupby(['entry_symbol_1', 'entry_symbol_2']).size()
-
-c_return_per_trade = last_c_return / number_of_trades
-c_return_per_trade.name = 'c_return_per_trade'
-data = data.merge(c_return_per_trade, left_on=['entry_symbol_1', 'entry_symbol_2'], right_index=True)
-
 data.drop('Unnamed: 0', inplace=True, axis=1)
+
+data['pair'] = data.entry_symbol_1 + '_' + data.entry_symbol_2
+
+data['long'] = (data['exit_price_1'] - data['entry_price_1']) * 100
+data['short'] = -(data['exit_price_2'] - data['entry_price_2']) * 100
+
+data['c_long'] = data.groupby('pair')['long'].cumsum()
+data['c_short'] = data.groupby('pair')['short'].cumsum()
+
+data['return'] = data['long'] + data['short']
+
+data['c_returns'] = data['c_long'] + data['c_short']
+
+last_c_return = data.groupby('pair').apply(lambda x: x['c_returns'].tail(1)).droplevel(1)
+number_of_trades = data.groupby('pair').size()
+c_return_per_trade = last_c_return / number_of_trades
+
+c_return_per_trade.name = 'c_return_per_trade'
+
+data = data.merge(c_return_per_trade, left_on='pair', right_index=True)
 
 data['duration'] = data['exit_time'] - data['entry_time']
 
+duration_trades_median = data.groupby('pair').apply(lambda x: x['duration'].median())
+duration_trades_median.name = 'duration_trades_median'
+
+duration_trades_mean = data.groupby('pair').apply(lambda x: x['duration'].mean())
+duration_trades_mean.name = 'duration_trades_mean'
+
+data = data.merge(duration_trades_mean, left_on='pair', right_index=True)
+data = data.merge(duration_trades_median, left_on='pair', right_index=True)
+
 data = data[
-    ['long', 'short', 'c_long', 'c_short', 'return', 'duration', 'last_return', 'c_return', 'c_return_per_trade']]
+    ['pair', 'long', 'short', 'c_long', 'c_short', 'return', 'duration', 'last_return',
+     'c_return', 'c_return_per_trade', 'duration_trades_mean', 'duration_trades_median']]
 
 data = data.sort_values('c_return', ascending=False)
 
@@ -44,6 +51,15 @@ avg_20 = data.head(20).mean()
 
 result = pd.DataFrame([average, avg_positive, avg_20, num_positive],
                       index=['avg', 'positive_avg', 'first_20_avg', 'return_positive'])
+
+# Her trade için ayrı
+
+# avg_per_pair = data.groupby('pair').mean()
+# avg_positive_trade_per_pair = data[data.c_return > 0].groupby('pair').mean()
+# positive_trade_number_per_pair = data[data.c_return > 0].groupby('pair').size()
+#
+#
+
 
 # duration_trades_median = data.groupby(['entry_symbol_1', 'entry_symbol_2']).apply(
 #     lambda x: x['duration'].median())
@@ -74,15 +90,13 @@ result = pd.DataFrame([average, avg_positive, avg_20, num_positive],
 #
 #
 #
-
+#
 # import pandas as pd
 #
 # data = pd.read_csv('../rol300_noint_5min_thr1_std.csv')
 #
 # data.duration_trades_median = pd.to_timedelta(data.duration_trades_median)
 # data.duration_trades_mean = pd.to_timedelta(data.duration_trades_mean)
-#
-#
 #
 # data = data.sort_values('c_return', ascending=False)
 #
@@ -95,6 +109,3 @@ result = pd.DataFrame([average, avg_positive, avg_20, num_positive],
 # avg_20 = data.head(20).mean()
 #
 # result = pd.DataFrame([average, avg_positive, avg_20, num_positive], index=['avg', 'positive', 'avg_20', 'num'])
-#
-#
-#
